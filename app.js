613107c13a15b9b369572fabf7d9ff8d26e23da6
Louis Valiquette <valiquette.louis@gmail.com>
2015-03-17 15:11:01 -0400

    6/49

--- app.js
+++ app.js
@@ -52,23 +52,82 @@
 //}
 
 
-var liste = [];
+//var listeEpicerie = [];
 
 
-while(true) {
+//while(true) {
 
 
-    liste.push(prompt("Ajoutez un item dans la liste d'épicerie"));
+ //   listeEpicerie.push(prompt("Ajoutez un item dans la listeEpicerie d'épicerie"));
 
-    if (liste == ["fini"]) {
-        alert("Voici tous les items dans la liste:");
-        for (var numeroliste = 0; numeroliste < liste.length; numeroliste++);
-        alert("item #" + (numeroliste + 1) + ": " + liste[numeroliste]);
-        break;
+   // if (listeEpicerie[listeEpicerie.length - 1] == ["fini"]) {
+     //   alert("Voici tous les items dans la listeEpicerie:");
+       // for (var numeroliste = 0; numeroliste < listeEpicerie.length; numeroliste++) {
+         //   alert("item #" + (numeroliste + 1) + ": " + listeEpicerie[numeroliste]);
+       // }
+       // break;
+   // }
+
+   // else {
+     //   alert("La listeEpicerie contient " + listeEpicerie.length + " items.");
+   // }
+
+//}
+
+function nombreAleatoire(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var i = 1;
+var numeroListe = [];
+var bonneListe = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49"];
+
+
+while (i <= 6) {
+    var numerochoisi = prompt("Choississez un numéro entre 1 et 49.");
+
+    if ((numerochoisi < 1) || (numerochoisi > 49)) {
+        alert("SVP recommencez");
+        i = (i - 1);
     }
 
     else {
-        alert("La liste contient " + liste.length + " items.");
+        numeroListe.push(numerochoisi);
+        bonneListe.indexOf(numerochoisi);
     }
 
+i = (i + 1);
 }
+
+console.log(numeroListe);
+
+//arr.indexOf(élémentRecherché[, indiceDébut = 0])
+//var indices = [];
+//var tableau = ['a', 'b', 'a', 'c', 'a', 'd'];
+//var élément = 'a';
+//var idx = tableau.indexOf(élément);
+//while (idx != -1) {
+  //  indices.push(idx);
+   // idx = tableau.indexOf(élément, idx + 1);
+//}
+//console.log(indices);
+// [0, 2, 4]
+//var listeEpicerie = [];
+//while(true) {
+  //  var nouvelItem = prompt("Ajoutez un item, entrez 'FINI' pour terminer.");
+    //if (nouvelItem.length == 0) {
+      //  alert("vous n'avez rien entré");
+    //}
+    //else if (nouvelItem == 'FINI') {
+        // afficher tous les items de la liste
+      //  alert("Voici tous les items dans la liste:");
+        //for (var numeroliste = 0; numeroliste < listeEpicerie.length; numeroliste++) {
+          //  alert("item #" + (numeroliste + 1) + ": " + listeEpicerie[numeroliste]);
+        //}
+        //break;
+    //}
+    //else {
+      //  listeEpicerie.push(nouvelItem);
+       // alert('La liste contient ' + listeEpicerie.length + ' items.');
+   // }
+//}
